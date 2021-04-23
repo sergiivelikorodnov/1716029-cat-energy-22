aedbf8cf5b9dd00e17fcec698cfd16bab207348e
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-04-23 16:26:25 +0200

    Добавил приезд меню

---
+++ source/js/script.js
@@ -0,0 +1,44 @@
+let navMain = document.querySelector('.main-nav');
+let topHeader = document.querySelector('.page-header__top');
+let navToggle = document.querySelector('.main-nav__toggle');
+let siteList = document.querySelector('.site-list');
+let openTrue = true;
+let clientHeight = navMain.clientHeight;
+
+
+navMain.classList.remove('main-nav--nojs');
+siteList.style.top = -clientHeight - 65 + 'px';
+
+
+/* navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+}); */
+window.addEventListener('resize', function (event) {
+  if (document.body.clientWidth > 767) {
+    openTrue = true;
+    topHeader.style.height = '65px';
+    siteList.style.top = -clientHeight - 65 + 'px';
+    navMain.classList.remove('main-nav--opened');
+  }
+})
+
+navToggle.addEventListener('click', function () {
+  navMain.classList.toggle('main-nav--opened');
+
+  if (openTrue) {
+    topHeader.style.height = clientHeight + 65 + 'px';
+    siteList.style.top = '0px';
+    openTrue = false;
+  } else {
+    topHeader.style.height = '65px';
+    siteList.style.top = -clientHeight - 65 + 'px';
+    openTrue = true;
+  }
+}
+)
