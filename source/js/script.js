b2168cc3c5ea958416eedd203c0175d12a611af8
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-04-26 20:59:29 +0200

    Автоматизация

--- source/js/script.js
+++ source/js/script.js
@@ -5,20 +5,9 @@ let siteList = document.querySelector('.site-list');
 let openTrue = true;
 let clientHeight = navMain.clientHeight;
 
-
 navMain.classList.remove('main-nav--nojs');
 siteList.style.top = -clientHeight - 65 + 'px';
 
-
-/* navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-}); */
 window.addEventListener('resize', function (event) {
   if (document.body.clientWidth > 767) {
     openTrue = true;
